ee04f442b019842280bd7dde6e12dcfd34653bbf
‪Ahmed Hamed‬‏ <128772653+AhmedHamed408@users.noreply.github.com>
2023-11-10 22:53:04 +0200

    Create Task-2-0-3.c

---
+++ Task-2-0-3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int A , B , C , D , X;
+
+    printf("Enter Numbers\n");
+    
+        printf("Enter A : ");
+        scanf("%d",&A);
+
+        printf("Enter B : ");
+        scanf("%d",&B);
+
+        printf("Enter C: ");
+        scanf("%d",&C);
+        
+        printf("Enter D: ");
+        scanf("%d",&D);
+        
+        X=(A*B)-(C*D);
+        
+        printf("X = %d ",X );
+
+}
