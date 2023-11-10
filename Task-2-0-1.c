9d8ee88c083a71fbaec07d5a49856a39ed01e4c7
‪Ahmed Hamed‬‏ <128772653+AhmedHamed408@users.noreply.github.com>
2023-11-10 22:36:49 +0200

    Create Task-2-0-1.c

---
+++ Task-2-0-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float A, R;
+    printf("Enter reduis : ");
+    scanf("%f" , &R);
+    A = (22.0/7.0)*R*R;
+    printf("Area = %f " , A);
+}
