46444cfec9b951368db601e7e50e4b8f8ebe5943
Ahmedhamed408 <ah456408@gmail.com>
2024-02-18 23:47:32 +0200

    Task

--- Task 10/Task_10_4.c
+++ Task 10/Task_10_4.c
@@ -5,5 +5,35 @@
 int main()
 {
 
+    int factorial( int factnum);
+    int num;
+    int Resault ;
+
+    printf("enter the number = ");
+    scanf("%d",&num);
+
+
+
+    int factresult = factorial(num);
+    printf("the factorial is = %d1\n",factresult );
+
     return 0;
+}
+
+
+
+int factorial( int factnum)
+{
+    int Resault =0;
+
+    if (factnum == 0)
+    {
+        Resault = 1;
+    }
+    else
+    {
+        Resault = factnum * factorial(factnum - 1);
+    }
+
+    return Resault;
 }
