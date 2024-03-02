e44dfccd3aba14c85016573966e684cfd84eeb31
Ahmedhamed408 <ah456408@gmail.com>
2024-03-02 14:46:14 +0200

    Edit Task 12

--- Task 12/Task_12_3.c
+++ Task 12/Task_12_3.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 #define Max_Students 50
 
@@ -12,7 +13,7 @@ int main()
 {
     student_t student[Max_Students];
     int studentnum, i, numhigh =0 ;
-    char highstudent[Max_Students];
+    student_t highstudent[Max_Students];
     float totscore = 0, avgscore = 0, highscore = 0;
     printf("Enter number of student (max: %d ): ", Max_Students);
     scanf("%d", &studentnum);
@@ -20,45 +21,63 @@ int main()
     for (i = 0; i < studentnum; i++)
     {
         printf("\nEnter details for student %d:\n", (i+1));
-        printf("%i - Name: ",(i+1));
+        printf("%d - Name: ",(i+1));
         scanf(" %s", student[i].name);
-        printf("%i - Score: ",(i+1));
+        printf("%d - Score: ",(i+1));
         scanf("%f", &student[i].score);
 
-        if (student[i].score > highscore)
+        if (student[i].score >= highscore)
         {
-            highscore = student[i].score;
-            numhigh = 1;
-            strcpy(highstudent[0], student[i].name);
-        }
-        else if (student[i].score == highscore)
-        {
-            strcpy(highstudent[numhigh], student[i].name);
+            highscore= student[i].score;
+            highstudent[numhigh].score = student[i].score;
+            strcpy(highstudent[numhigh].name, student[i].name);
             numhigh++;
+
         }
+        totscore+=student[i].score;
+
     }
 
     avgscore = totscore / studentnum;
+    printf("\n=============================\n");
 
-    printf("avrage is %f \n", avgscore);
+    printf("avrage is %0.2f \n", avgscore);
+    printf("=============================\n");
 
-    printf("student above avrage: ");
+    printf("\n\n\n=============================\n");
+    printf("student Below avrage: ");
 
-    for (int i = 0; i < studentnum; ++i)
+    for (int i = 0; i < studentnum; i++)
     {
         if (student[i].score < avgscore)
         {
-            printf("%s , ", student[i].name);
+            printf("%s   ", student[i].name);
         }
     }
+    printf("\n=============================\n");
 
 
-    printf("High score: %f\n", highscore);
-    printf(" high sstudents:");
+    printf("\nHigh score: %0.2f \n", highscore);
+    printf("\n=============================\n");
+
+    printf("high students");
+    printf("\n=============================\n");
+
+
+    printf("student Name      Score\n");
+    printf("------------      -----\n");
+
+
+
     for (int i = 0; i < numhigh; ++i)
     {
-        printf("%s ,", highstudent[i]);
+
+        printf("%s                   %0.2f\n", highstudent[i].name,highstudent[i].score);
     }
+
+    printf("\n=============================\n");
+
+    return 0;
 
 }
 
